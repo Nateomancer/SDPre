917380db7a891c5ecd88a44cded77d2aae9fff97
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-02-22 15:57:21 -0700

    complete objective1lab1

---
+++ Labs/Objective1Lab1.java
@@ -0,0 +1,12 @@
+//Objective1Lab1
+//Nathan Hafley
+
+public class Objective1Lab1 {
+  public static void main(String[] args) {
+
+    //Output Hello, Skill Distillery
+    System.out.println("Hello, Skill Distillery");
+
+
+  }
+}
