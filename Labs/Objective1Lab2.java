cff8cec622d298815de6b3ba777e0919de53c2a4
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-02-22 16:23:09 -0700

    complete objective1lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,13 @@
+//Objective2Lab1
+//Nathan Hafley
+
+public class Objective1Lab2 {
+  public static void main(String[] args) {
+
+    //Output grocery list
+    System.out.println("Grocery List");
+    System.out.println("1. Milk");
+    System.out.println("2. Eggs");
+    System.out.println("3. Bread");
+  }
+}
