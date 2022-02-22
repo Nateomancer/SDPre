4166d5ce287848296cddee28c950f9621b4f7bf4
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-02-22 16:52:14 -0700

    complete objective2lab1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,22 @@
+//Objective2Lab1
+//Nathan Hafley
+
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+
+    //Example1
+    //String hello = "Hello World";
+    //Contains decleration and initialization
+
+
+    //Lab Exercise
+    //Declare Variables
+    String hello;
+    //Initilize  Variables
+    hello ="Hello World";
+
+    //Output String
+    System.out.println(hello);
+
+  }
+}
