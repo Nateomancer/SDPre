642d9c940ce66208d7af872f8ecfe9b6522d0d3c
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-02-22 17:40:01 -0700

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,24 @@
+//Objective 2 Lab 3 - Writing Expressions
+//Nathan Hafley
+
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    //Declare Variables
+    double num1, num2, num3, average;
+
+    //Initilize Variables
+    num1 =15;
+    num2=6;
+    num3=2;
+
+    //Find average
+    average=num1+num2+num3;
+
+    average=average/3;
+
+    //Output average
+    System.out.println(average);
+
+  }
+}
