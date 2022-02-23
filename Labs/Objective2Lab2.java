66687e3542294b39b875d033bdd6aa6631e7a39f
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-02-22 17:21:20 -0700

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,18 @@
+//Objective2Lab2 - Printing Multiple Variables
+//Nathan Hafley
+
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+
+    //Decalre and Initilize Variables
+    String name = "Nathan Hafley";
+    int age = 29;
+    String state = "Michigian";
+
+    //Output vairables
+    System.out.println("Name: "+name+" Age: "+age+" State: "+state);
+    //+ name + " Age: " + age " State: " + state);
+
+
+  }
+}
