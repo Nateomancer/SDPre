8ed187524a99d8cbfb7f23215b222168fcf13579
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-02-23 17:05:31 -0700

    complete objective1lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,24 @@
+//Objective3Lab1 - Writing expressions
+//Nathan Hafley
+
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+
+    //declare variables
+    double num1, num2, num3, average;
+
+    //initialize variables
+    num1=15;
+    num2=6;
+    num3=2;
+
+    //Find average
+    average=num1+num2+num3;
+    average=average/3;
+
+    //Output average
+    System.out.println(average);
+
+
+  }
+}
