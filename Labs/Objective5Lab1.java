6ed375b3fc4024f9242c784ed2517f3335c095e5
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-02-27 13:44:23 -0700

    complete objective1lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,25 @@
+//Objective5Lab1-Comparing two numbers with if statements
+//Nathan Hafley
+
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+
+    //Declare Variables
+    int num1 = 5;
+    int num2 = 15;
+
+    //first conditional statement
+    if(num1 > num2) {
+
+      System.out.println(num1+ " is greater than "+ num2);
+
+    }
+
+    //Second conditional statement
+    else if(num2 > num1) {
+
+      System.out.println(num2+ " is greater than "+ num1);
+
+    }
+  }
+}
