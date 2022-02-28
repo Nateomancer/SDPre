db7c4ad066309fde5c7735d2be36887a47c9a25a
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-02-28 13:52:05 -0700

    complete objective7lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,25 @@
+//Objective7Lab1-Loops Print numbers in reverse order
+//Nathan Hafley
+
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+
+    //Declare counter variable for loop
+    int counter = 10;
+
+
+    //Create  while loop
+    while(counter > 0) {
+
+
+      //Outputs current counter int
+      System.out.println(counter);
+
+
+      //At the end of the while loop, counter will be decreaed by 1
+      counter = counter - 1;
+
+    }//End of loop after this bracket
+
+  }
+}
