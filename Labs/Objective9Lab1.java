6146528ccda38779e28651f4255aaed003656f4e
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-03-01 19:35:42 -0700

    Complete oObjective9lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,15 @@
+//Objective9Lab1-Printing from a method
+//Nathan Hafley
+
+public class Objective9Lab1 {
+  public static void main(String[] args){
+
+  //Call print name method
+  printName();
+
+  }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
