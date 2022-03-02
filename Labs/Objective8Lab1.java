d252c3a169b1dfe2b4b248e04b1b873871ec7c19
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-03-01 18:42:43 -0700

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,24 @@
+//Objective8Lab1-Printing numbers For with for loops
+//Nathan Hafley
+
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    //Declare counter i varaiable
+    int i=1;
+
+    //For loop that counts from 1-10
+    for (i = 1; i <= 10; i++){
+
+      System.out.println(i);
+
+      }
+
+      //For loop that counts from 10=1
+      for (i=10;i>=1; i--){
+
+      System.out.println(i);
+
+      }
+ }
+}
