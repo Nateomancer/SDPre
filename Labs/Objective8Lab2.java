aa462fd54f6320ef63ec3bc57ed21bf0bab27999
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-03-01 19:03:01 -0700

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,20 @@
+//Objective8Lab2-find the sum using a for loop
+
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+
+    //Declare counter i varaiable
+    int i=1;
+    int iSum=0;
+
+    //For loop that adds that adds the control variable to the sum
+    for (i = 1; i <= 20; i++){
+
+      iSum=iSum+i;
+      }
+
+      //Output sum
+      System.out.println(iSum);
+
+ }
+}
