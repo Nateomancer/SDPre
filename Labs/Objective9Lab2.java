c8be54057c3e4d831d29b9bf74a87dde14bf7a81
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-03-01 19:43:01 -0700

    Complete Objective9Lab2

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,18 @@
+//Objective9Lab2-Print message from method 2
+//Nathan Hafley
+
+public class Objective9Lab2 {
+
+
+  public static void main(String[] args) {
+    System.out.println("In main");
+    printMessage();
+    System.out.println("In main");
+  }
+
+  public static void printMessage() {
+
+    System.out.println("From Method");
+
+  }
+}
